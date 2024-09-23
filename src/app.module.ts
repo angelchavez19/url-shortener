e83849ca8b428603f21e25b23ce482574658f7d1
angelchavez19 <infoangelchavez@gmail.com>
2024-09-22 21:52:56 -0500

    add- Image

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'static') }),
+    UrlModule,
+  ],
 })
 export class AppModule {}
